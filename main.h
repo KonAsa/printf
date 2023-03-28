d103740b0263896c38adc443bb1f01c57587dccd
KonAsa <konas32@gmail.com>
2023-03-27 17:26:19 -0700

    main.h

--- main.h
+++ main.h
@@ -30,7 +30,6 @@ struct fmt
 	int (*fn)(va_list, char[], int, int, int, int);
 };
 
-
 /**
  * typedef struct fmt fmt_t - Struct op
  *
@@ -115,3 +114,4 @@ long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 
 #endif /* MAIN_H */
+
